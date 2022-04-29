f08c766c7eef826d3821cbc9973705350cbc50f7
prarathod <prajwal_fw11_331@masai.school>
2022-04-29 15:34:10 +0530

    connectiong to dynamic data

--- client/src/container/About/About.jsx
+++ client/src/container/About/About.jsx
@@ -2,19 +2,27 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 // import { AppWrap, MotionWrap } from '../../wrapper';
+import { images } from '../../constants/index'
 import './About.scss';
-import { urlFor, client } from '../../client';
+// import { urlFor, client } from '../../client';
 
 const About = () => {
-  const [abouts, setAbouts] = useState([]);
+  // const [abouts, setAbouts] = useState([]);
 
-  useEffect(() => {
-    const query = '*[_type == "abouts"]';
+  // useEffect(() => {
+  //   const query = '*[_type == "abouts"]';
 
-    client.fetch(query).then((data) => {
-      setAbouts(data);
-    });
-  }, []);
+  //   client.fetch(query).then((data) => {
+  //     setAbouts(data);
+  //   });
+  // }, []);
+
+  const abouts = [
+    { title: "Web Development", description: "I am a good Web developer", imgUrl: images.about01 },
+    { title: "React Development", description: "I am a good React Development", imgUrl: images.about02 },
+    { title: "Backend Development", description: "I am a good Backend developer", imgUrl: images.about03 },
+    { title: "MERN Stack", description: "I am a good MERN Stack", imgUrl: images.about04 }
+  ]
 
   return (
     <>
@@ -29,7 +37,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <img src={about.imgUrl} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
